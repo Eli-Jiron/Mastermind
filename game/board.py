afd78e4eb7ecf365352ac945af34c47c6122916a
Eli-Jiron <elianguevara28@gmail.com>
2024-08-06 02:29:08 -0600

    print board class

--- game/board.py
+++ game/board.py
@@ -1,1 +1,11 @@
-+
+class Board:
+    def __init__(self, board: list[list]) -> None:
+        self.__board = board
+
+    def print_board(self):
+        print("--------------------")
+        for row in self.__board:
+            for e in row:
+                print("\a", e, end="")
+            print()
+        print("--------------------")
